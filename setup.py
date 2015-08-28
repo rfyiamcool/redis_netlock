c46db6d30783c9eebb35846612746189cb663740
芮峰云 <rfyiamcool@163.com>
2015-08-28 15:50:35 +0800

    match pypi

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@
 #!/usr/bin/env python
 
 import os
-import ez_setup
-ez_setup.use_setuptools()
 
 from setuptools import setup
 
@@ -16,7 +14,7 @@ def list_files(path):
             yield fn
 
 setup(name='redis_netlock',
-      version = '1.1',
+      version = '1.5',
       author="ruifengyun",
       author_email="rfyiamcool@163.com",
       url="http://xiaorui.cc/",
@@ -33,27 +31,5 @@ setup(name='redis_netlock',
       license="BSD",
       keywords='redis lock distributed',
       description="一个用redis做的分布式锁，含有超时及重试机制.",
-      long_description="""\
-redis_netlock
----------------
-
-Implements a distributed transaction using redis or
-a redis compatible storage.
-
-
-Example
--------
-
-Basic example of usage::
-
-from __future__ import with_statement
-import redis
-import time
-from redis_netlock import dist_lock
-
-client = redis.Redis(connection_pool=redis.BlockingConnectionPool(max_connections=15, host='localhost', port=6379))
-with dist_lock('test', client):
-    time.sleep(10)
-    print 'welcome to my blog, http://xiaorui.cc'
-
-""")
+      long_description=open('README.md').read(),
+)
